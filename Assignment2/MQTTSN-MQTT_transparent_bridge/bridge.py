702214c5956509c96506114812f3fe086bcb076f
Emanuele <62481322+Lelito-IAIA@users.noreply.github.com>
2020-04-01 16:03:45 +0200

    Add files via upload

--- Assignment2/MQTTSN-MQTT_transparent_bridge/bridge.py
+++ Assignment2/MQTTSN-MQTT_transparent_bridge/bridge.py
@@ -1,36 +1,36 @@
-import MQTTSNclient as mqttsn
-import mqtt_client as mqtt
-import time
+import MQTTSNclient as mqttsn												#Import of MQTTSNclient
+import mqtt_client as mqtt													#Import of mqtt_client
+import time																	#Import of time
     									
-class RecivingCallback1:								
-	def messageArrived(self, topicName, payload, qos, retained, msgid):		
+class RecivingCallback1:													#Callback that allows to publish the payload of the first
+	def messageArrived(self, topicName, payload, qos, retained, msgid):		#virtual environment station to the Thingsboard Broker
 	 print "Payload arrived for Station1"
 	 ret= mclient.publish1(payload) 			
 	 return True
     									
-class RecivingCallback2:							
-	def messageArrived(self, topicName, payload, qos, retained, msgid):		
+class RecivingCallback2:													#Callback that allows to publish the payload of the second
+	def messageArrived(self, topicName, payload, qos, retained, msgid):		#virtual environment station to the Thingsboard Broker
 	 print "Payload arrived for Station2"
 	 ret= mclient.publish2(payload) 			
 	 return True
 
-mclient = mqtt.MQTT_client()
+mclient = mqtt.MQTT_client()												#Defining of a new mqtt_client object
 
-aclient1 = mqttsn.Client("linh1", port=1885)
-aclient1.registerCallback(RecivingCallback1())
-aclient2 = mqttsn.Client("linh2", port=1885)
-aclient2.registerCallback(RecivingCallback2())
+aclient1 = mqttsn.Client("linh1", port=1885)								#Defining of the first MQTTSNclient listening on 1885 port  
+aclient1.registerCallback(RecivingCallback1())								#and assigning to it the first callback function
+aclient2 = mqttsn.Client("linh2", port=1885)								#Defining of the second MQTTSNclient listening on 1885 port
+aclient2.registerCallback(RecivingCallback2())								#and assigning to it the second callback function
 print("Connecting to MQTT-SN Broker:")
-aclient1.connect()
-aclient2.connect()
-aclient1.subscribe("devices/vs1")
-aclient2.subscribe("devices/vs2")
-print("#CONNECTED TO MQTT-SN BROKER#\n")
+aclient1.connect()															#Connecting the first MQTTSNclient
+aclient2.connect()															#Connecting the second MQTTSNclient
+aclient1.subscribe("devices/vs1")											#Subscribing the first MQTTSNclient to the VS1's topic
+aclient2.subscribe("devices/vs2")											#Subscribing the first MQTTSNclient to the VS2's topic
+print("#CONNECTED TO MQTT-SN BROKER#\n")	
 print("# READY TO BRIDGE #\n")
 
 
-try:
-	while(1):
+try:																		#Loop to keep the bridge listening until a keyinterrupt
+	while(1):																# is inserted
 		time.sleep(2)
 except KeyboardInterrupt:
 	print("Closing the client")
--- Assignment2/MQTTSN-MQTT_transparent_bridge/mqtt_client.py
+++ Assignment2/MQTTSN-MQTT_transparent_bridge/mqtt_client.py
@@ -1,36 +1,37 @@
-import paho.mqtt.client as paho  		    				
+import paho.mqtt.client as paho  		    									#Import of paho-mqtt library	
 
+#Defining the MQTT_client class that will be able to connect to the ThingsBoard broker
 class MQTT_client:
 	def __init__(self,ACCESS_TOKEN1='m4tTwRDNRpt3tWy7yokT', ACCESS_TOKEN2="j2QZULfsKWRMIlVsO9Ni",broker="demo.thingsboard.io",port=1883):
 		print("Connecting to MQTT Thingsboard Broker:")
-		self.client1= paho.Client(client_id="control1")
-		self.client2= paho.Client(client_id="control1")
+		self.client1= paho.Client(client_id="control1")							#Create client1 object
+		self.client2= paho.Client(client_id="control1")							#Create client2 object
 		print("#")                    			
-		self.client1.on_publish = self.on_publish 
-		self.client2.on_publish = self.on_publish 
+		self.client1.on_publish = self.on_publish 								#Assign function to callback of client1
+		self.client2.on_publish = self.on_publish 								#Assign function to callback of client2
 		print("#")                     					
-		self.client1.username_pw_set(ACCESS_TOKEN1)
-		self.client2.username_pw_set(ACCESS_TOKEN2) 
+		self.client1.username_pw_set(ACCESS_TOKEN1)		#Assaign to client1 the access token of  Virtual Environment Station 1 in ThingsBoard
+		self.client2.username_pw_set(ACCESS_TOKEN2) 	#Assaign to client1 the access token of  Virtual Environment Station 2 in ThingsBoard
 		print("#")              						
-		self.client1.connect(broker,port,keepalive=60)  
-		self.client2.connect(broker,port,keepalive=60) 
+		self.client1.connect(broker,port,keepalive=60)  			#Establish connection between client1 and ThingsBoard
+		self.client2.connect(broker,port,keepalive=60) 				#using the func connect from the class client
 		print("#")          					
 		print("#CONNECTED MQTT THINGSBOARD BROKER#")
 
-	def on_publish(client,userdata,result):
+	def on_publish(client,userdata,result):													#Callback Function
 		print("data published to MQTT Thingsboard Broker \n")
 		pass
     	
-	def publish1(self,payload): 
+	def publish1(self,payload): 															#Send the payload 1 to client1 topic
 		ret= self.client1.publish("v1/devices/me/telemetry",payload) 
 		print(payload)		
 		print("Please check LATEST TELEMETRY field of Station1\n")
 	
-	def publish2(self,payload): 
+	def publish2(self,payload): 															#Send the payload 2 to client2 topic
 		ret= self.client2.publish("v1/devices/me/telemetry",payload) 
 		print(payload)		
 		print("Please check LATEST TELEMETRY field of Station2\n")
 		
-   	def disconnect(self):
+   	def disconnect(self):																	#Disconnect from the ThingsBoard Broker
    		self.client.disconnect()
    		print("disconnected from MQTT broker")
