7d272418842e77b549314f289c9a46dc41027703
Emanuele <62481322+Lelito-IAIA@users.noreply.github.com>
2020-04-09 19:06:00 +0200

    Update main.c

--- Assignment3/LoRaWANStation/Station1/main.c
+++ Assignment3/LoRaWANStation/Station1/main.c
@@ -1,11 +1,3 @@
-/*
- * Copyright (C) 2018 Inria
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
 #include <string.h>
 
 #include "xtimer.h"
@@ -29,6 +21,7 @@ static hts221_t hts221;
 static const uint8_t deveui[LORAMAC_DEVEUI_LEN] = { 0x00, 0x27, 0x1A, 0x6D, 0x44, 0x82, 0x8D, 0x72 };
 static const uint8_t appeui[LORAMAC_APPEUI_LEN] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x02, 0xD4, 0x49 };
 static const uint8_t appkey[LORAMAC_APPKEY_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, };                  //Personal device appkey 
+
 //Generating Random Values
 int get_random(int min, int max) { 
     int randValue = (rand() % (max - min + 1)) + min;
