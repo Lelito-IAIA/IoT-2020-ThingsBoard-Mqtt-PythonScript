ddca4f6cbdc844f7d343979788d19ff14d54db39
Emanuele <62481322+Lelito-IAIA@users.noreply.github.com>
2020-04-09 16:23:55 +0200

    Update main.c

--- Assignment3/LoRaWANStation/Station1/main.c
+++ Assignment3/LoRaWANStation/Station1/main.c
@@ -28,8 +28,7 @@ static hts221_t hts221;
 /* Device and application informations required for OTAA activation */
 static const uint8_t deveui[LORAMAC_DEVEUI_LEN] = { 0x00, 0x27, 0x1A, 0x6D, 0x44, 0x82, 0x8D, 0x72 };
 static const uint8_t appeui[LORAMAC_APPEUI_LEN] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x02, 0xD4, 0x49 };
-static const uint8_t appkey[LORAMAC_APPKEY_LEN] = { 0x83, 0x5F, 0xD0, 0x1D, 0x19, 0x77, 0x81, 0xB0, 0xA0, 0xE3, 0x20, 0x9F, 0x02, 0xCA, 0x1A, 0xD0 };
-
+static const uint8_t appkey[LORAMAC_APPKEY_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, };                  //Personal device appkey 
 //Generating Random Values
 int get_random(int min, int max) { 
     int randValue = (rand() % (max - min + 1)) + min;
@@ -51,26 +50,14 @@ void get_payload(char* payload){
 static void sender(void)
 {
     while (1) {
-        //char message[32];
+        
         /* sleep 20 secs */
         xtimer_sleep(20);
 
-        /* do some measurements */
-        //uint16_t humidity = 0;
-        //int16_t temperature = 0;
+        /* Generating the payload with random values */
+       
         char payload[400];
         get_payload(payload);
-        /*if (hts221_read_humidity(&hts221, &humidity) != HTS221_OK) {
-            puts(" -- failed to read humidity!");
-        }
-        if (hts221_read_temperature(&hts221, &temperature) != HTS221_OK) {
-            puts(" -- failed to read temperature!");
-        }
-            
-        sprintf(message, "H: %u.%u, T:%u.%u",
-                (humidity / 10), (humidity % 10),
-                (temperature / 10), (temperature % 10));
-        */
         printf("Sending data: %s\n", payload);
 
         /* send the LoRaWAN message */
